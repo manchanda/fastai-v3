d5c170c3da791dc2e70ac48b40b9fea7cfc602a1
Sanjeev Manchanda <ai.sanjeevm@gmail.com>
2019-07-10 09:32:32 +0530

    Update server.py
    changing to mango classifier.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1bFak9nRy9R-GxZYfJoB_Q-Oj2byB8WCj?alt=media&key=AIzaSyC_1i9i8eyxf1YShaxWE5rYTMISk0Y_1sc'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+# classes = ['black', 'grizzly', 'teddys']
+classes = ['banganpalli','sinduri','alphonso','totapuri']
 path = Path(__file__).parent
 
 app = Starlette()
